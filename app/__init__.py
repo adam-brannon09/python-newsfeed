cd41b8c80e931702760547fe6f3eb1f990382740
Adam Brannon <adambrannon@Adams-MacBook-Pro.local>
2023-08-14 23:01:36 -0500

    setup routes and pages. issue#1 completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from app.routes import home
+from app.routes import home, dashboard
+
 
 def create_app(test_config=None):
   # set up app config
@@ -11,5 +14,9 @@ def create_app(test_config=None):
   @app.route('/hello')
   def hello():
     return 'hello there world'
+  
+  # register routes
+  app.register_blueprint(home)
+  app.register_blueprint(dashboard)
 
   return app
